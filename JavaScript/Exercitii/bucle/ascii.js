af28acd0a6f7b08f2533953937125f66c3ce65ab
JavaScript <javascript@Admins-iMac.local>
2019-12-12 20:52:18 +0200

    Exercitii - bucle

---
+++ JavaScript/Exercitii/bucle/ascii.js
@@ -0,0 +1,12 @@
+// var code = 0;
+
+// while(code <= 255) {
+//     document.write(code + " " + String.fromCharCode(code));
+//     document.write("<br>");
+//     code++;
+// }
+
+for(var code = 0; code <= 255; code++) {
+    document.write(code + " " + String.fromCharCode(code));
+    document.write("<br>");
+}
