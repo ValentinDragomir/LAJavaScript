daf58aa43820a9f1e6c241c1c4da89a974c216c2
ValentinDragomir <valentindragomir@live.com>
2020-04-06 20:30:48 +0300

    Advanced JavaScript 2

---
+++ JavaScript/Exercitii/vue/hello.js
@@ -0,0 +1,32 @@
+Vue.component(
+    'hello',
+    {
+        'template': "<div>Hello World!</div>"
+    }
+)
+
+var app = new Vue({
+    el: '#app', // elementul pe care il controleaza Vue
+    data: {     // datele aplicatiei (model)
+        message: "Hello Vue!",
+        // password: "",
+        // score: 0,
+        // message: 'very weak',
+        show: true,
+        todos: [
+            "Learn Java Script",
+            "Learn Vue.JS",
+            "Learn React",
+            "Be free!"
+        ],
+        title: "This is a welcome message"
+    },
+    methods: {
+        reverse: function() {
+            this.message = this.message
+                           .split('')
+                           .reverse()
+                           .join('');
+        }
+    }
+});
